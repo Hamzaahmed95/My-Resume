5a46de4fb23e6d5b9b01d2d9ccec1aff2539fa62
Hamza Ahmed <hamza.ahmed0402@gmail.com>
2020-05-19 03:24:20 +0800

    Updates

---
+++ precache-manifest.db505fc4d8d6fc88c854ad320033b247.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1ccacab3fedc38ddc1b3fd7bb8a942",
+    "url": "/My-Resume/index.html"
+  },
+  {
+    "revision": "bdbd34fd7fe7363432d3",
+    "url": "/My-Resume/static/css/main.36d810f6.chunk.css"
+  },
+  {
+    "revision": "9a15019a1b5d5bc2c024",
+    "url": "/My-Resume/static/js/2.a3caf061.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Resume/static/js/2.a3caf061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbd34fd7fe7363432d3",
+    "url": "/My-Resume/static/js/main.e4390b2b.chunk.js"
+  },
+  {
+    "revision": "333cd136558d15512c69",
+    "url": "/My-Resume/static/js/runtime-main.f60d41eb.js"
+  },
+  {
+    "revision": "8789f465f48f57b467f5f909d5e56ba6",
+    "url": "/My-Resume/static/media/aboutme.8789f465.png"
+  },
+  {
+    "revision": "01190d4810ed128355c1e95289f6c910",
+    "url": "/My-Resume/static/media/achievement1.01190d48.jpeg"
+  },
+  {
+    "revision": "6338c1071f1441e3fb4218540bc33791",
+    "url": "/My-Resume/static/media/achievement2.6338c107.jpeg"
+  },
+  {
+    "revision": "ebbef7b4dad87f9d1a359dd2e380f5c8",
+    "url": "/My-Resume/static/media/achievement3.ebbef7b4.jpeg"
+  },
+  {
+    "revision": "e621d4d3a51c9840ff3d076fe91a8b3e",
+    "url": "/My-Resume/static/media/android.e621d4d3.png"
+  },
+  {
+    "revision": "4041acc88da75a24d46b83e3f571060d",
+    "url": "/My-Resume/static/media/blog11.4041acc8.png"
+  },
+  {
+    "revision": "59a6ebb21115d43fa1a4c8280b9b094c",
+    "url": "/My-Resume/static/media/blog22.59a6ebb2.png"
+  },
+  {
+    "revision": "58a18f79180320d23afef8529ea30fb2",
+    "url": "/My-Resume/static/media/blog33.58a18f79.png"
+  },
+  {
+    "revision": "a68c39fc8cd3d778f21d68cc384990fe",
+    "url": "/My-Resume/static/media/blog44.a68c39fc.png"
+  },
+  {
+    "revision": "8494811ea7773d336d374b9af27f0c30",
+    "url": "/My-Resume/static/media/blog55.8494811e.png"
+  },
+  {
+    "revision": "1916d2e269778ce10179cd5102041088",
+    "url": "/My-Resume/static/media/github.1916d2e2.png"
+  },
+  {
+    "revision": "590c5909cbef9846212ffa137b48c709",
+    "url": "/My-Resume/static/media/linkedin.590c5909.png"
+  },
+  {
+    "revision": "293ca4e9f8883810fb81f41791cad2b5",
+    "url": "/My-Resume/static/media/medium.293ca4e9.png"
+  },
+  {
+    "revision": "3731eca15df42b3bfe040ab770952547",
+    "url": "/My-Resume/static/media/node.3731eca1.png"
+  },
+  {
+    "revision": "a4f7fbb465ee2339425cb0fb593f6703",
+    "url": "/My-Resume/static/media/picture1.a4f7fbb4.png"
+  },
+  {
+    "revision": "d10efb8f56146782ac3e3439b9766166",
+    "url": "/My-Resume/static/media/portfolio.d10efb8f.png"
+  },
+  {
+    "revision": "8e6e7af5482439b9d97a1a65ecbf4021",
+    "url": "/My-Resume/static/media/profile.8e6e7af5.png"
+  },
+  {
+    "revision": "93bf0379ca2aeb82b02ddd9e575b4fc3",
+    "url": "/My-Resume/static/media/react.93bf0379.png"
+  },
+  {
+    "revision": "ede7d35efc3a6bf629a8da593ad6dbdf",
+    "url": "/My-Resume/static/media/twitter.ede7d35e.png"
+  }
+]);
