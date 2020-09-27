72090324f3a1b55738b97653f6aa5aebda331bec
Hamza Ahmed <hamza.ahmed0402@gmail.com>
2020-09-28 05:28:44 +0800

    save all files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,7 +13,6 @@ class App extends Component {
       about_me: true,
       achievements: false,
       blogs: false,
-      events: false,
       project:false,
       toggle: false,
       projects: false,
@@ -21,7 +20,6 @@ class App extends Component {
     this.handleChangeAboutMe = this.handleChangeAboutMe.bind(this)
     this.handleChangeAchievements = this.handleChangeAchievements.bind(this)
     this.handleChangeBlogs = this.handleChangeBlogs.bind(this)
-    this.handleChangeEvents = this.handleChangeEvents.bind(this)
     this.handleChangeToggle = this.handleChangeToggle.bind(this)
     this.handleChangeProjects = this.handleChangeProjects.bind(this)
 
@@ -32,7 +30,6 @@ class App extends Component {
       achievements: false,
       about_me: true,
       blogs: false,
-      events: false,
       projects:false
     })
   }
@@ -41,7 +38,6 @@ class App extends Component {
       achievements: true,
       about_me: false,
       blogs: false,
-      events: false,
       projects:false
     })
   }
@@ -50,26 +46,15 @@ class App extends Component {
       achievements: false,
       about_me: false,
       blogs: true,
-      events: false,
       projects:false
     })
   }
 
-  handleChangeEvents() {
-    this.setState({
-      achievements: false,
-      about_me: false,
-      blogs: false,
-      events: true,
-      projects:false
-    })
-  }
   handleChangeProjects() {
     this.setState({
       achievements: false,
       about_me: false,
       blogs: false,
-      events: false,
       projects:true,
     })
   }
@@ -83,8 +68,8 @@ class App extends Component {
     return (
       <div className="container1">
         
-        <div style={this.state.toggle? sidebar_background_dark: sidebar_background_light} className="AboutMe"><Sidebar about_me1={this.state.about_me} blogs1={this.state.blogs} achievements1={this.state.achievements}  events1={this.state.events} toggle1={this.state.toggle} projects1={this.state.projects} about_me={this.handleChangeAboutMe} achievements={this.handleChangeAchievements} blogs={this.handleChangeBlogs} events={this.handleChangeEvents} toggle={this.handleChangeToggle} projects={this.handleChangeProjects} /></div>
-        <div style={this.state.toggle? main_component_background_dark:main_component_background_light} className="Intro"><MainComponent about_me={this.state.about_me} blogs={this.state.blogs} achievements={this.state.achievements}  events={this.state.events} toggle={this.state.toggle} projects={this.state.projects}/></div>
+        <div style={this.state.toggle? sidebar_background_dark: sidebar_background_light} className="AboutMe"><Sidebar about_me1={this.state.about_me} blogs1={this.state.blogs} achievements1={this.state.achievements}  toggle1={this.state.toggle} projects1={this.state.projects} about_me={this.handleChangeAboutMe} achievements={this.handleChangeAchievements} blogs={this.handleChangeBlogs} toggle={this.handleChangeToggle} projects={this.handleChangeProjects} /></div>
+        <div style={this.state.toggle? main_component_background_dark:main_component_background_light} className="Intro"><MainComponent about_me={this.state.about_me} blogs={this.state.blogs} achievements={this.state.achievements}  toggle={this.state.toggle} projects={this.state.projects}/></div>
       </div>
     );
   }
