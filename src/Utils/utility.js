b694502770d58a197c8b423e4f19800937aeec49
Hamza Ahmed <hamzaahmed@Hamzas-Mac-mini.local>
2023-06-07 17:31:54 +0800

    added all export in one file

--- src/Utils/utility.js
+++ src/Utils/utility.js
@@ -1,25 +1,6 @@
 
 import React from 'react'
-import Profile from '../images/profile.jpg'
 import { useThemeContext } from '../hooks/useThemeContext'
-
-export const DisplayProfilePicture = () => (
-  <div align="center">
-    <img className="profile_picture" src={Profile} width="200" height="250" />
-  </div>
-)
-
-export const ToggleButton = () => {
-  const theme = useThemeContext()
-  return (
-    <div className="toggle_button list">
-      <label className="switch">
-        <input onChange={theme.toggleTheme} type="checkbox" />
-        <span className="slider round"></span>
-      </label>
-    </div>
-  )
-}
 
 export const getThemeStyle = (className) => {
   const theme = useThemeContext()
